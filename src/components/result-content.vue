3030c550ebc38409a130663f0a568896c0625ca5
y-fukumoto <helmutlang1028@gmail.com>
2018-04-16 23:10:20 +0900

    edit gitignore

--- src/components/result-content.vue
+++ src/components/result-content.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="main__result">
-    <div class="result__info">
-      <p class="result__title">ページタイトル：{{state.scrapingData.title}}</p>
-      <p class="result__url"><a :href="state.scrapingData.url">{{state.scrapingData.url}}</a></p>
-      <p class="result__input">タグ：<input type="text" v-model="tag"><a @click="addTag()">タグ追加</a></p>
-      <p class="result__tag" v-for="tag in tags" :key="tag.id">{{tag}}</p>
-      <a href="javascript:void(0)" @click="saveDesign" class="result__save">保存する</a>
+  <div class="result">
+    <div class="result__body">
+      <div class="result__info">
+        <p class="result__title">ページタイトル：{{state.scrapingData.title}}</p>
+        <p class="result__url"><a :href="state.scrapingData.url">{{state.scrapingData.url}}</a></p>
+        <p class="result__input">タグ：<input type="text" v-model="tag"><a @click="addTag()">タグ追加</a></p>
+        <p class="result__tag" v-for="tag in tags" :key="tag.id">{{tag}}</p>
+        <a href="javascript:void(0)" @click="saveDesign" class="result__save btn red lighten-1">保存する</a>
+      </div>
+      <div class="result__images">
+        <img :src="'/images/' + state.scrapingData.image" alt="" class="result__image">
+      </div>
     </div>
-    <img :src="'/images/' + state.scrapingData.image" alt="" class="result__image">
   </div>
 </template>
 
@@ -53,20 +57,39 @@ export default {
 </script>
 
 <style scoped>
+.result {
+  position: fixed;
+  background-color: rgba(255,255,255,.4);
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 10;
+}
+
+.result__body {
+  overflow-y: scroll;
+  width: 80%;
+  max-height: 100%;
+  margin: 50px auto;
+  background-color: white;
+}
+
 .result__info {
   float: left;
+  width: 30%;
 }
 
 .result__images {
   float: left;
+  width: 70%;
+}
+
+.result__image {
+  max-width: 100%;
 }
 
 .result__save {
-  display: block;
-  width: 100px;
-  height: 40px;
-  line-height: 40px;
-  text-align: center;
-  border: 1px solid #efefef;
+  color: white;
 }
 </style>
