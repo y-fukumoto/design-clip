f6e63104fcf54226acd87ef76eb4cc17406b1028
y-fukumoto <helmutlang1028@gmail.com>
2018-04-22 01:56:18 +0900

    一旦完成

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,8 @@
 'use strict';
 const Sequelize = require('sequelize');
+const config = require('../config')
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/clipDesign',
+  process.env.DATABASE_URL || config.DB,
   { logging: true }
 );
 
