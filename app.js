cef4a6857a2879c9f54b54e59d8a538569503d95
y-fukumoto <helmutlang1028@gmail.com>
2018-04-22 20:38:05 +0900

    Google Authの設定を外部ファイル化

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8080/auth/google/callback"
+  callbackURL: config.GOOGLE_CALLBACK
 },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function() {
